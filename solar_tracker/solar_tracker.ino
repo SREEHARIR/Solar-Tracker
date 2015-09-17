baf264b0da3239504859cc3aa24536988215df8e
R SREEHARI <sreehari.r96@gmail.com>
2015-09-17 18:48:52 +0530

    Setup LDR

--- solar_tracker/solar_tracker.ino
+++ solar_tracker/solar_tracker.ino
@@ -7,6 +7,13 @@ int servoh ;//= 90;     // stand horizontal servo
 Servo vertical;   // vertical servo 
 int servov ;//= 90;     // stand vertical servo
 
+// LDR pin connections
+//  name  = analogpin;
+int ldrlt = 2; //LDR top left
+int ldrrt = 3; //LDR top rigt
+int ldrld = 4; //LDR down left
+int ldrrd = 5; //ldr down rigt
+
 void setup()
 {
   Serial.begin(9600);
@@ -19,4 +26,15 @@ void setup()
 
 void loop() 
 {
+
+  int lt = analogRead(ldrlt); // top left
+  int rt = analogRead(ldrrt); // top right
+  int ld = analogRead(ldrld); // down left
+  int rd = analogRead(ldrrd); // down rigt
+    
+  int avt = (lt + rt) / 2; // average value top
+  int avd = (ld + rd) / 2; // average value down
+  int avl = (lt + ld) / 2; // average value left
+  int avr = (rt + rd) / 2; // average value right
+  
 }
